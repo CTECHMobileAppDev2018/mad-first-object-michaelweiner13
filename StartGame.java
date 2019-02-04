d1dda38b6fbfaf0f19e1e43962a5cf1de9adf891
Michael Weiner <46925721+michaelweiner13@users.noreply.github.com>
2019-02-04 09:48:53 -0600

    Update StartGame.java

--- StartGame.java
+++ StartGame.java
@@ -2,6 +2,9 @@ public class StartGame {
   
   public static void main(String[] args) {
    
+    // *******************
+    // ******Players******
+    // *******************
     // Create a player for Mr. B
     Player player1 = new Player();
     player1.setName("Mr. Bartucz");
@@ -11,8 +14,6 @@ public class StartGame {
     
     //Print out player1's health using getHealth()
     System.out.println(player1.getName() + "'s health is: " + player1.getHealth());
-    
-    
     
     // Create a player for Michael
     Player player2 = new Player();
@@ -24,8 +25,17 @@ public class StartGame {
     //Print out player2's health using getHealth()
     System.out.println(player2.getName() + "'s health is: " + player2.getHealth());
     
+    
+    // *******************
+    // ******Weapons******
+    // *******************
+    // Create a new weapon object for a new weapon called "Gun"
     Weapon gun = new Weapon();
+    
+    // Set the name of the new weapon to the Gun
     gun.setName ("Gun");
+    
+    // Print out the details of the gun to the user using getName() and getDamageDealt()
     System.out.println("The weapon: " + gun.getName() + " has been addded to the game it does " + gun.getDamageDealt() + " damage to player's health.");
   }  
 }
