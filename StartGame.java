cfa9c6415303d9fe265c49dc4e1cbb9b255b1868
Michael <6993102@isd535.org>
2019-02-04 15:11:00 -0600

    Update StartGame.java

--- StartGame.java
+++ StartGame.java
@@ -1,46 +1,51 @@
 public class StartGame {
-  
+
   public static void main(String[] args) {
-   
+
     // *******************
     // ******Players******
     // *******************
     // Create a player for Mr. B
     Player player1 = new Player();
     player1.setName("Mr. Bartucz");
-    
+
     // Print out player1's name using getName()
     System.out.println("Player1 name is: " + player1.getName());
-   
+    System.out.println();
+
     //Print out player1's health using getHealth()
     System.out.println(player1.getName() + "'s health is: " + player1.getHealth());
-    
-    
+    System.out.println();
+
     // Create a player for Michael
     Player player2 = new Player();
     player2.setName("Michael");
-    
+
     // Print out player1's name using getName()
     System.out.println("Player2 name is: " + player2.getName());
-    
+    System.out.println();
+
     //Print out player2's health using getHealth()
     System.out.println(player2.getName() + "'s health is: " + player2.getHealth());
-   
-    
+    System.out.println();
+
+
     // *******************
     // ******Weapons******
     // *******************
     // Create a new weapon object for a new weapon called "Gun"
     Weapon gun = new Weapon();
     gun.setName ("Gun"); // Set the name of the new weapon to the Gun
-    
+
     // Print out the details of the gun to the user using getName() and getDamageDealt()
-    System.out.println("The weapon: " + gun.getName() + " has been addded to the game. It does " + gun.getDamageDealt() + " damage to player's health.");
-    
-    
+    System.out.println("The weapon - " + gun.getName() + " - has been addded to the game. It does " + gun.getDamageDealt() + " damage to player's health.");
+    System.out.println();
+
     // *******************
     // ******Actions******
     // *******************
     gun.damagePlayer(player2);
-  }  
+    gun.damagePlayer(player2);
+    gun.damagePlayer(player2);
+  }
 }
