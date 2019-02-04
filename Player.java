ab37b710beda5f27efee900617a4d87c833fb415
Michael <6993102@isd535.org>
2019-02-04 08:42:12 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,35 +1,53 @@
-
 // This is an example class for a player in a game
 public class Player {
   
-  // we don't want other objects changing these directly, so they are private
+  // Initalize variables for the player's name and health
+  // Set the variable to private to make sure the user cannot change the values
+  // Values can be changed through setName(), getName(), setHealth(), getHealth()
   private String name;
   private int health;
   
-  // this is called a "constructor". It is automatically called when someone creates a new Player object
+  // Constructor
+  // Called when someone creates a Player object
   public void Player() {
     this.health = 100;
   }
   
-  // next we have "setters" and "getters"
-  // these allow us to set and get properties, including doing some error checking
+  // Setter method for the player's name
   public void setName(String playerName) { 
 
-    // first let's make sure the length of the name is longer than 0
+    // Make sure the length of the name is longer than 0
     if (playerName.length() < 1) {
       this.name = "invalid";
       return;
     }
     
+    // If the player's new name meets the error checking, change the player's name to the user's input
     this.name = playerName;
   }
   
-  // this just gets the name 
+  // Getter method for the player's name
+  // Returns the player's name
   public String getName() {
     return this.name;
   }
   
-  // here is where you should insert your setHealth and getHealth methods
-  // what are some conditions that need to be checked?
+  // Setter method for the player's health
+  public void setHealth (int playerHealth) {
+    // Checks to make sure the player's health is not trying to be set to anything less than 0
+    if (playerHealth < 0) {
+      System.out.println("Your player's health cannot be less than 0. Your player's health is now 0.");
+      this.health = 0;
+    } else if (playerHealth > 100) { // Checks to make sure the player's health is not more than 100
+      System.out.println("Your player's health cannot be more than 100. Your player's health is now 100.");
+      this.health = 100;
+    } else { // Return the user's change to the player's health
+      this.health = playerHealth;
+    }
+  }
   
+  //Getter method for the player's health
+  public int getHealth() {
+    return this.health;
+  }
 }
